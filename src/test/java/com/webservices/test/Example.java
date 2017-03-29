b9e9a42cb0ff51e77aaf142e512308b4582ce8fa
Kakoti <Plabon.Kakoti@shell.com>
2017-03-29 14:42:17 +0200

    added a new method

--- src/test/java/com/webservices/test/Example.java
+++ src/test/java/com/webservices/test/Example.java
@@ -4,5 +4,11 @@ public class Example {
 	public static void main(String[] args){
 		
 		// add a new functional call here
+		getFibbonaci(15);
 	}
+	
+	public void getFibbonaci(int n){
+		if(n < 2)return 1;
+		return getFibbonaci(n1-1) + getFibbonaci(n1-2);
+
 }
